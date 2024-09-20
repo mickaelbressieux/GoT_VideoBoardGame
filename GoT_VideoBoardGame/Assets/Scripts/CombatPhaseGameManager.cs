3b81cc895433e6b1bca46abb2d98d8acd6d269f1
mickaelbressieux <mickael.bressieux@gmail.com>
2024-09-20 15:15:49 +0200

    working button

---
+++ GoT_VideoBoardGame/Assets/Scripts/CombatPhaseGameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombatPhaseGameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Combat Phase Game Manager Loaded");
+        // Link the button to the GameManager function "SwitchToStrategyPhase"
+        Button switchPhaseButton = GameObject.Find("SwitchPhaseButton").GetComponent<UnityEngine.UI.Button>();
+        Debug.Log(switchPhaseButton);
+        GameObject gameManager = GameObject.Find("GameManager");
+        Debug.Log(gameManager);
+        switchPhaseButton.onClick.AddListener(() => gameManager.GetComponent<GameManager>().SwitchToStrategyPhase());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
